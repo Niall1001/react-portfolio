905688f0779d8d38bbb95329bc14a5660fbdb892
Niall1001 <52977336+Niall1001@users.noreply.github.com>
2021-01-08 16:55:19 +0000

    Personal CV Website

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,54 +1,95 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Radium, { StyleRoot } from 'radium';
+import { headShake, fadeInLeftBig } from 'react-animations';
 
+const styles = {
+  bannerFadeIn: {
+    animationDuration: '3s',
+    animationName: Radium.keyframes(fadeInLeftBig, 'fadeInLeftBig'),
+  },
+  textFadeIn: {
+    animationDuration: '6s',
+    animationName: Radium.keyframes(fadeInLeftBig, 'fadeInLeftBig'),
+  },
+  imageFadeIn: {
+    animationDuration: '6s',
+    animationName: Radium.keyframes(fadeInLeftBig, 'fadeInLeftBig'),
+  },
+  socialFadeIn: {
+    animationDuration: '6s',
+    animationName: Radium.keyframes(headShake, 'headShake'),
+  },
+};
 
 class Landing extends Component {
   render() {
-    return(
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-          <Cell col={12}>
-            <img
-              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
-              alt="avatar"
-              className="avatar-img"
+    return (
+      <StyleRoot>
+        <div style={{ width: '100%', margin: 'auto' }}>
+          <Grid className='landing-grid'>
+            <Cell col={12}>
+              <img
+                src='https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png'
+                alt='avatar'
+                className='avatar-img'
               />
 
-            <div className="banner-text">
-              <h1>Full Stack Web Developer</h1>
+              <div className='banner-text' style={styles.bannerFadeIn}>
+                <h1 style={styles.textFadeIn}>
+                  Graduate Full Stack Web Developer
+                </h1>
 
-            <hr/>
+                <hr />
 
-          <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+                <p style={styles.textFadeIn}>
+                  HTML/CSS | Bootstrap | JavaScript | React | React Native |
+                  NodeJS | Express | MongoDB
+                </p>
 
-        <div className="social-links">
+                <div className='social-links' style={styles.imageFadeIn}>
+                  {/* LinkedIn */}
+                  <a
+                    href='http://google.com'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <i className='fa fa-linkedin-square' aria-hidden='true' />
+                  </a>
 
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
+                  {/* Github */}
+                  <a
+                    href='http://google.com'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <i className='fa fa-github-square' aria-hidden='true' />
+                  </a>
 
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square" aria-hidden="true" />
-          </a>
+                  {/* Freecodecamp */}
+                  <a
+                    href='http://google.com'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <i className='fa fa-free-code-camp' aria-hidden='true' />
+                  </a>
 
-          {/* Freecodecamp */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-free-code-camp" aria-hidden="true" />
-          </a>
-
-          {/* Youtube */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-youtube-square" aria-hidden="true" />
-          </a>
-
+                  {/* Facebook */}
+                  <a
+                    href='https://www.facebook.com/niall.walters/'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                  >
+                    <i className='fa fa-facebook-f' aria-hidden='true' />
+                  </a>
+                </div>
+              </div>
+            </Cell>
+          </Grid>
         </div>
-            </div>
-          </Cell>
-        </Grid>
-      </div>
-    )
+      </StyleRoot>
+    );
   }
 }
 
--- src/components/resume.js
+++ src/components/resume.js
@@ -4,94 +4,87 @@ import Education from './education';
 import Experience from './experience';
 import Skills from './skills';
 
-
 class Resume extends Component {
   render() {
-    return(
+    return (
       <div>
         <Grid>
           <Cell col={4}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: 'center' }}>
               <img
-                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
-                alt="avatar"
-                style={{height: '200px'}}
-                 />
+                src='https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png'
+                alt='avatar'
+                style={{ height: '200px' }}
+              />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
-            <h4 style={{color: 'grey'}}>Programmer</h4>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h2 style={{ paddingTop: '2em' }}>Niall Walters</h2>
+            <h4 style={{ color: 'grey' }}>Graduate Software Engineer</h4>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <p>
+              I present as an outgoing, social person, well-spoken and can be
+              pro-active and responsible in any graduate programming role,
+              handling pressure and stress who is intuitive and effective in
+              dealing with public, a hardworking individual working well within
+              a team with a good work ethic striving to complete tasks on time.
+            </p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
+            <p>5 Balfour Avenue, Belfast, BT7 2EU</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>07455 003158</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
+            <p>niallwalters135@gmail.com</p>
             <h5>Web</h5>
-            <p>mywebsite.com</p>
-            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>Aboutniallwalters.com</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
           </Cell>
-          <Cell className="resume-right-col" col={8}>
+          <Cell className='resume-right-col' col={8}>
             <h2>Education</h2>
 
+            <Education
+              startYear={2017}
+              endYear={2021}
+              schoolName='St Patricks Grammar School, Downpatrick'
+              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
 
             <Education
-              startYear={2002}
-              endYear={2006}
-              schoolName="My University"
+              startYear={2007}
+              endYear={2009}
+              schoolName='University Of Ulster - BSc (Hons) Computing Science'
               schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-               />
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
 
-               <Education
-                 startYear={2007}
-                 endYear={2009}
-                 schoolName="My 2nd University"
-                 schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                  />
-                <hr style={{borderTop: '3px solid #e22947'}} />
-
-              <h2>Experience</h2>
+            <h2>Experience</h2>
 
             <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
+              startYear={2019}
+              endYear={2020}
+              jobName='Placement at Citi'
               jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              />
+            />
 
-              <Experience
-                startYear={2012}
-                endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                />
-              <hr style={{borderTop: '3px solid #e22947'}} />
-              <h2>Skills</h2>
-              <Skills
-                skill="javascript"
-                progress={100}
-                />
-                <Skills
-                  skill="HTML/CSS"
-                  progress={80}
-                  />
-                  <Skills
-                    skill="NodeJS"
-                    progress={50}
-                    />
-                    <Skills
-                      skill="React"
-                      progress={25}
-                      />
-
-
+            <Experience
+              startYear={2012}
+              endYear={2016}
+              jobName='Second Job'
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Skills</h2>
+            <Skills skill='javascript' progress={80} />
+            <Skills skill='Java' progress={75} />
+            <Skills skill='Mockito' progress={50} />
+            <Skills skill='Angular' progress={50} />
+            <Skills skill='HTML/CSS' progress={60} />
+            <Skills skill='NodeJS' progress={50} />
+            <Skills skill='React' progress={50} />
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
